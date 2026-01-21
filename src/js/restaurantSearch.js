1af0e5d99d765231d98cf9a0f0be0076ac53cb0d
Majki03 <majki03@int.pl>
2026-01-21 17:12:59 +0100

    search fix

--- src/js/restaurantSearch.js
+++ src/js/restaurantSearch.js
@@ -1,13 +1,34 @@
-export function searchRestaurants(restaurantList, searchInput){
-    return restaurantList.filter((restaurant) => restaurant.restaurantName.toLowerCase().includes(searchInput.toLowerCase()));
-}
-
-export function filterRestaurantsByType(restaurantsList, type){
-    return restaurantsList.filter((restaurant) => restaurant.type.includes(type));
-}
-
-export function filterRestaurants(restaurantList, state){
+export function searchRestaurants(restaurantList, searchInput) {
     return restaurantList.filter((restaurant) =>
-            restaurant.restaurantName.toLowerCase().includes(state.searchFilter.trim()) && restaurant.category.includes(state.categoryFilter)
-        );
+        restaurant.restaurantName
+            .toLowerCase()
+            .includes(searchInput.toLowerCase()),
+    );
+}
+
+export function filterRestaurantsByType(restaurantsList, type) {
+    return restaurantsList.filter((restaurant) =>
+        restaurant.category.includes(type),
+    );
+}
+
+export function filterRestaurants(restaurantList, state) {
+    const searchTerm = state.searchFilter.trim().toLowerCase();
+
+    const selectedDropdownCategory = state.categoryFilter;
+
+    return restaurantList.filter((restaurant) => {
+        const matchesDropdown =
+            selectedDropdownCategory === '' ||
+            restaurant.category === selectedDropdownCategory;
+
+        const restaurantName = restaurant.restaurantName.toLowerCase();
+        const restaurantCategory = restaurant.category.toLowerCase();
+
+        const matchesSearchText =
+            restaurantName.includes(searchTerm) ||
+            restaurantCategory.includes(searchTerm);
+
+        return matchesDropdown && matchesSearchText;
+    });
 }
